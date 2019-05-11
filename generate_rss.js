5db4372e8c36a32d23a9da01d041041cc9852a6f
Martin Naumann <mr.avgp@gmail.com>
2019-05-11 18:41:07 +0000

    Using the HTML files for RSS

--- generate_rss.js
+++ generate_rss.js
@@ -16,12 +16,12 @@ for(var i=0; i<files.length;i++) {
   var title = content.match(/<\!\-\-([^<]+)\-\->/)[1].trim()
   var desc  = content.match(/##[^\n]+\n\n([^#]+)(?=\n\n)/)[1]
   if(!content.match(/<!-- DRAFT -->/)) {
-    items.push({title: title, description: striptags(desc), link: url + '/' + files[i].slice(0, -3) + '.html'})
+    items.push({title: title, description: striptags(desc), link: url + '/' + files[i].slice(0, -3)})
   }
 }
 
 items = items.reverse().map(function(item) {
-  return '<item>\n\t<title>' + item.title + '</title>\n\t<link>' + item.link + '</link>\n\t<description>' + item.description + '</description>\n\t<guid isPermaLink="true">' + item.link + '</guid>\n</item>'
+  return '<item>\n\t<title>' + htmlEntities(item.title) + '</title>\n\t<link>' + item.link + '</link>\n\t<description>' + htmlEntities(item.description) + '</description>\n\t<guid isPermaLink="true">' + item.link + '</guid>\n</item>'
 })
 
 var feedContent = fs.readFileSync('rss-template.xml', 'utf8')
@@ -29,3 +29,7 @@ feedContent = feedContent.replace('{{date}}', new Date().toUTCString())
 feedContent = feedContent.replace('{{items}}', items.join('\n'))
 
 fs.writeFileSync('rss.xml', feedContent)
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
